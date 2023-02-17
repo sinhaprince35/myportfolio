cd8673608329ab16a9f422f9af1bf4edd267a6e1
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-02-17 22:37:44 +0530

    Create ProjectContainer.js

---
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -0,0 +1,51 @@
+import uniqid from 'uniqid'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LaunchIcon from '@material-ui/icons/Launch'
+import './ProjectContainer.css'
+
+const ProjectContainer = ({ project }) => (
+  <div className='project'>
+    <div>
+    <img className="projectImage" src={project.image} alt={project.name}/>
+    </div>
+
+    <div>
+    <h3>{(project.name).toUpperCase()}</h3>
+    
+    
+    <p className='project__description'>{project.description}</p>
+    {project.stack && (
+      <ul className='project__stack'>
+        {project.stack.map((item) => (
+          <li key={uniqid()} className='project__stack-item'>
+            {item}
+          </li>
+        ))}
+      </ul>
+    )}
+
+    {project.sourceCode && (
+      <a
+        href={project.sourceCode} target='_blank' rel="noreferrer"
+        aria-label='source code'
+        className='link link--icon'
+      >
+        <GitHubIcon />
+      </a>
+    )}
+
+    {project.livePreview && (
+      <a
+        href={project.livePreview} target='_blank' rel="noreferrer"
+        aria-label='live preview'
+        className='link link--icon'
+      >
+        <LaunchIcon />
+      </a>
+    )}
+    </div>
+    
+  </div>
+)
+
+export default ProjectContainer
